b470ab35945875c184c5b31a9ba39fa43e3d27bb
Hemant Kumar <hemantkumar@MacBook-Pro.local>
2022-10-24 00:59:17 +0530

    Refactoring deterministicPartitionKey method

--- dpk.js
+++ dpk.js
@@ -1,26 +1,36 @@
 const crypto = require("crypto");
 
+const getCandidate = (event)=>{
+
+    let candidate;
+
+    if (event) {
+        if (event.partitionKey) {
+          candidate = event.partitionKey;
+        } else {
+          const data = JSON.stringify(event);
+          candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+        }
+      }
+
+      return candidate;
+}
+
+const makeJsonString = (candidate)=>{
+    if (typeof candidate !== "string") {
+          candidate =  JSON.stringify(candidate);
+        }
+    return candidate
+}
+
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
 
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
-  }
+  let candidate = getCandidate(event) || TRIVIAL_PARTITION_KEY;
 
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
-  } else {
-    candidate = TRIVIAL_PARTITION_KEY;
-  }
+  candidate = makeJsonString(candidate)
+
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
     candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
   }
