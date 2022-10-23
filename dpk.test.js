ccd4cbead67f2ee85b3aef491f51b3ed42b131a6
Hemant Kumar <hemantkumar@MacBook-Pro.local>
2022-10-24 00:49:23 +0530

    Adding test cases

--- dpk.test.js
+++ dpk.test.js
@@ -6,3 +6,32 @@ describe("deterministicPartitionKey", () => {
     expect(trivialKey).toBe("0");
   });
 });
+
+test("deterministicPartitionKey String", () => {
+    const crypto = require("crypto");
+    const event = {
+        partitionKey : "coderbyte"
+    }
+    let res = "coderbyte";
+    expect(deterministicPartitionKey(event)).toBe(res);
+})
+
+test("deterministicPartitionKey json ", () => {
+    const crypto = require("crypto");
+    const event = {
+        fname : "Hemant",
+        lname : "kumar"
+    }
+    const data = JSON.stringify(event);
+    candidate = crypto.createHash("sha3-512").update(data).digest("hex");
+    expect(deterministicPartitionKey(event)).toBe(candidate);
+})
+
+test("deterministicPartitionKey number ", () => {
+    const crypto = require("crypto");
+    const event = {
+        partitionKey : "23333"
+    }
+    let res = "23333";
+    expect(deterministicPartitionKey(event)).toBe(res);
+})
